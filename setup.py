2ee22d29da59ddc6d7f0def6c24f3e423bea0771
mrkeith99 <kvalentincardenas@hotmail.com>
2026-05-12 21:12:22 +0900

    [FIX] entry pointsa

--- setup.py
+++ setup.py
@@ -24,7 +24,11 @@ setup(
     maintainer_email='atarbabgei@gmail.com',
     description='A ROS 2 package to simulate joystick inputs with a keyboard.',
     license='MIT',
-    tests_require=['pytest'],
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         'console_scripts': [
             'joy_node = keyboard_joy.joy_node:main', 
